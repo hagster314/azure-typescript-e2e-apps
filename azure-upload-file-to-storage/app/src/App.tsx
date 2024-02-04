5b48d58fb5388ba323a81a768b36220cb5b1fd89
hagster314 <77243645+hagster314@users.noreply.github.com>
2024-02-04 12:54:11 +0000

    2ndtry

--- azure-upload-file-to-storage/app/src/App.tsx
+++ azure-upload-file-to-storage/app/src/App.tsx
@@ -1,13 +1,12 @@
 import { BlockBlobClient } from '@azure/storage-blob';
 import { Box, Button, Card, CardMedia, Grid, Typography } from '@mui/material';
-import { ChangeEvent, useState, useEffect } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import ErrorBoundary from './components/error-boundary';
 import { convertFileToArrayBuffer } from './lib/convert-file-to-arraybuffer';
 
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import './App.css';
 
-// Environment variable for the API server URL
 const API_SERVER = import.meta.env.VITE_API_SERVER as string;
 
 const request = axios.create({
@@ -17,156 +16,105 @@ const request = axios.create({
   },
 });
 
-type SasResponse = {
-  url: string;
-};
-type ListResponse = {
-  list: string[];
-};
-
 function App() {
   const containerName = `upload`;
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [uploadStatus, setUploadStatus] = useState<string>('');
   const [list, setList] = useState<string[]>([]);
 
-  const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-
-    if (!(target instanceof HTMLInputElement)) return;
-    if (
-      target.files === null ||
-      target.files.length === 0 ||
-      target.files[0] === null
-    )
-      return;
-
-    setSelectedFile(target.files[0]);
-    setUploadStatus('');
-  };
-
-  const getFileSasToken = async (fileName: string): Promise<string> => {
-    const permission = 'w'; // Write permission
-    const timerange = 5; // Valid time range in minutes
-    
-    try {
-      const response = await request.post<SasResponse>(
-        `/api/sas?file=${encodeURIComponent(
-          fileName
-        )}&permission=${permission}&container=${containerName}&timerange=${timerange}`
-      );
-      
-      return response.data.url;
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error(`Error getting SAS token: ${error.message}`);
-      } else {
-        console.error('Unknown error getting SAS token.');
-      }
-      throw error;
-    }
-  };
-
-  const handleFileUpload = async () => {
-    if (!selectedFile) return;
-
-    try {
-      const sasTokenUrl = await getFileSasToken(selectedFile.name);
-      const fileArrayBuffer = await convertFileToArrayBuffer(selectedFile);
-      
-      if (fileArrayBuffer === null || fileArrayBuffer.byteLength < 1) {
-        setUploadStatus('File is empty or too large');
-        return;
-      }
-
-      const blockBlobClient = new BlockBlobClient(sasTokenUrl);
-      await blockBlobClient.uploadData(fileArrayBuffer);
-      setUploadStatus('Successfully finished upload');
-
-      const result = await request.get<ListResponse>(`/api/list?container=${containerName}`);
-      setList(result.data.list);
-    } catch (error) {
-      if (error instanceof Error) {
-        setUploadStatus(`Upload failed: ${error.message}`);
-      } else {
-        setUploadStatus('Upload failed with an unknown error.');
-      }
-    }
-  };
-
-  // Function to load the images list on component mount
   useEffect(() => {
-    const fetchImages = async () => {
-      try {
-        const result = await request.get<ListResponse>(`/api/list?container=${containerName}`);
-        setList(result.data.list);
-      } catch (error) {
-        console.error('Failed to fetch images list.');
-      }
-    };
     fetchImages();
   }, []);
 
+  const fetchImages = async () => {
+    try {
+      const result = await request.get(`/api/list?container=${containerName}`);
+      const { data } = result;
+      setList(data.list);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  }
+
+  const handleFileSelection = (event: ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    if (!(target instanceof HTMLInputElement)) return;
+    if (target?.files === null || target?.files?.length === 0 || target?.files[0] === null) return;
+
+    setSelectedFile(target?.files[0]);
+    setUploadStatus('');
+  };
+
+  const handleFileUpload = () => {
+    if (!selectedFile) return;
+    
+    const getUploadSasUrl = async () => {
+      const permission = 'w'; //write
+      const timerange = 5; //minutes
+      try {
+        const result = await request.post(`/api/sas?file=${encodeURIComponent(selectedFile.name)}&permission=${permission}&container=${containerName}&timerange=${timerange}`);
+        return result.data.url;
+      } catch (error) {
+        console.error('Error getting SAS Token:', error);
+        throw error;
+      }
+    };
+
+    const uploadFileWithSas = async (sasUrl: string) => {
+      const fileArrayBuffer = await convertFileToArrayBuffer(selectedFile);
+      if (!fileArrayBuffer) throw new Error('File conversion failed or file is empty.');
+      
+      const blockBlobClient = new BlockBlobClient(sasUrl);
+      await blockBlobClient.uploadData(fileArrayBuffer);
+    };
+
+    getUploadSasUrl()
+      .then(uploadFileWithSas)
+      .then(() => {
+        setUploadStatus('Successfully finished upload');
+        fetchImages(); // Fetch images again to update the list
+      })
+      .catch((error) => {
+        const errorMessage = error instanceof Error ? `Failed to finish upload: ${error.message}` : "Upload failed.";
+        setUploadStatus(errorMessage);
+      });
+  };
+
   return (
-    <ErrorBoundary>
-      <Box m={4}>
-        <Typography variant="h4" gutterBottom>
-          Upload file to Azure Storage
-        </Typography>
-        <Typography variant="h5" gutterBottom>
-          with SAS token
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-          <b>Container: {containerName}</b>
-        </Typography>
+    <>
+      <ErrorBoundary>
+        <Box m={4}>
+          <Typography variant="h4" gutterBottom>Upload file to Azure Storage</Typography>
+          <Typography variant="h5" gutterBottom>with SAS token</Typography>
+          <Typography variant="body1" gutterBottom><b>Container: {containerName}</b></Typography>
 
-        <Box display="block" justifyContent="left" alignItems="left" flexDirection="column" my={4}>
-          <Button variant="contained" component="label">
-            Select File
-            <input type="file" hidden onChange={handleFileSelection} />
-          </Button>
-          {selectedFile && selectedFile.name && (
-            <Box my={2}>
-              <Typography variant="body2">{selectedFile.name}</Typography>
-            </Box>
-          )}
-        </Box>
-
-        {selectedFile && (
           <Box display="block" justifyContent="left" alignItems="left" flexDirection="column" my={4}>
-            <Button variant="contained" onClick={handleFileUpload}>
-              Upload
+            <Button variant="contained" component="label">Select File
+            <input type="file" hidden onChange={handleFileSelection} />
             </Button>
-            {uploadStatus && (
-              <Box my={2}>
-                <Typography variant="body2" gutterBottom>
-                  {uploadStatus}
-                </Typography>
+            {selectedFile && selectedFile.name && 
+              <Box my={2}><Typography variant="body2">{selectedFile.name}</Typography></Box>
+            }
+            {selectedFile && (
+              <Box display="block" justifyContent="left" alignItems="left" flexDirection="column" my={4}>
+                <Button variant="contained" onClick={handleFileUpload}>Upload</Button>
+                {uploadStatus && <Box my={2}><Typography variant="body2" gutterBottom>{uploadStatus}</Typography></Box>}
               </Box>
             )}
           </Box>
-        )}
 
-        <Grid container spacing={2}>
-          {list.map((item, index) => (
-            <Grid item xs={6} sm={4} md={3} key={index}>
-              <Card>
-                {item.endsWith('.jpg') ||
-                item.endsWith('.png') ||
-                item.endsWith('.jpeg') ||
-                item.endsWith('.gif') ? (
-                  <CardMedia component="img" image={`${API_SERVER}/${containerName}/${item}`} alt={item} />
-                ) : (
-                  <Typography variant="body1" gutterBottom>
-                    {item}
-                  </Typography>
-                )}
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-    </ErrorBoundary>
+          <Grid container spacing={2}>
+            {list.map((item) => (
+              <Grid item xs={6} sm={4} md={3} key={item}>
+                <Card>
+                  <CardMedia component="img" image={item} alt={item} />
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </ErrorBoundary>
+    </>
   );
 }
 
